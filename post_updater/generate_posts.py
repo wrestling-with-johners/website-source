a2cc2c0de807265119d60106359205b5878e5b6a
Matthew Padbury <mpforce1@gmail.com>
2021-04-22 08:31:07 +0100

    Updated apple auth

--- post_updater/generate_posts.py
+++ post_updater/generate_posts.py
@@ -192,7 +192,7 @@ class AppleScraper:
       'Referer': 'https://podcasts.apple.com/us/podcast/id{}'.format(APPLE_PODCAST_ID),
       'Origin': 'https://podcasts.apple.com',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-      'Authorization': 'Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkNSRjVITkJHUFEifQ.eyJpc3MiOiI4Q1UyNk1LTFM0IiwiaWF0IjoxNjE2MDEwMTg4LCJleHAiOjE2MTkwMzQxODh9.guWJzIakaf6RQzFm_f3MNgwg2mCPV1JVSeIQOTZAa-RmFqB8kqpiYPFRSLwxtYD7_9B66wMNvHwd2XYPMyTFWA',
+      'Authorization': 'Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkNSRjVITkJHUFEifQ.eyJpc3MiOiI4Q1UyNk1LTFM0IiwiaWF0IjoxNjE4NTIxMjc1LCJleHAiOjE2MjE1NDUyNzV9.rNMJhzGyHLBx2G9RawqZrlN5sLtTw7AZNSKmykfLf-cSO9WwYvN0r196EQT98SLnVCS1fV3C_7Eo9xI8w3sTBg',
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
     })
     if response.ok:
